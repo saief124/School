cca044ab0a17b4d8a485b18f0c0ca4f03af95ed8
saief124 <saiefsayed123@hotmail.com>
2021-01-27 13:20:12 -0600

    update routes

--- backend/School/config/routes.rb
+++ backend/School/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :course_students, only: [:index, :create, :destroy]
   resources :courses, only: [:index, :create, :update, :destroy]
   resources :students, only: [:index, :create, :destroy, :update]
-  resources :instructors, only: [:index]
-  resources :notes
+  resources :instructors, only: [:index, :create]
+  resources :notes, only: [:index, :create, :destroy]
   post '/login', to: 'sessions#create'
   post '/studentlogin', to: 'student_sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
